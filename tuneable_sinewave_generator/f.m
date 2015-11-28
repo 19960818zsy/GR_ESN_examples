081675c478123d11cb526daca75e8301b6ebb70c
Andreea Ciuprina <andreea.c1110@gmail.com>
2015-11-28 16:11:04 +0100

    Revert "add tunable swgen"
    This reverts commit f9a090d62b5318e7ff24b88fe37c204ca0211a17.

--- tuneable_sinewave_generator/f.m
+++
@@ -1,5 +0,0 @@
-function y = f(x)
-%activation function for neuron activation update, set to Tanh(x)or
-%identity or any other invertible sigmoid you want
-y = tanh(x);
-%  y = x;
--- tuneable_sinewave_generator/minusPoint5.m
+++
@@ -1,3 +0,0 @@
-function x = minusPoint5(y)
-% a little helper used in generateNet, just subtracts 0.5
-x = y - 0.5;
